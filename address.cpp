04d694093bf501e326243c9d07bd334f2ddd4222
Lana <115147586+ricorett@users.noreply.github.com>
2024-01-30 20:18:08 +0300

    add bold file

---
+++ address.cpp
@@ -0,0 +1,6 @@
+#include "address.hpp"
+
+address::address()
+{
+
+}
---
+++ address.hpp
@@ -0,0 +1,11 @@
+#ifndef ADDRESS_HPP
+#define ADDRESS_HPP
+
+
+class address
+{
+public:
+    address();
+};
+
+#endif // ADDRESS_HPP
